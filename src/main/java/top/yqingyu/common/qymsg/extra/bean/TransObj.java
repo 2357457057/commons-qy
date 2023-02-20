5701112f70c6bcc373ffb68d665089c07abdb157
Severus <yangyongji123@live.com>
2023-02-20 17:27:37 +0800

    TransObj.java

---
+++ src/main/java/top/yqingyu/common/qymsg/extra/bean/TransObj.java
@@ -0,0 +1,49 @@
+package top.yqingyu.common.qymsg.extra.bean;
+
+public class TransObj {
+    private String fileName;
+    private long size;
+    private boolean isUpload = false;
+    private String sendPath;
+    private String savePath;
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
+
+    public boolean isUpload() {
+        return isUpload;
+    }
+
+    public void setUpload(boolean upload) {
+        isUpload = upload;
+    }
+
+    public String getSendPath() {
+        return sendPath;
+    }
+
+    public void setSendPath(String sendPath) {
+        this.sendPath = sendPath;
+    }
+
+    public String getSavePath() {
+        return savePath;
+    }
+
+    public void setSavePath(String savePath) {
+        this.savePath = savePath;
+    }
+}

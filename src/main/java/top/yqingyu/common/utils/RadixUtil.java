a1ee9cac5550af47d9638ad1e6082ad579ecc3ad
SeverusQy <yangyongji123@live.con>
2023-06-28 04:08:42 +0800

    进制工具

--- src/main/java/top/yqingyu/common/utils/RadixUtil.java
+++ src/main/java/top/yqingyu/common/utils/RadixUtil.java
@@ -16,14 +16,34 @@ public class RadixUtil {
             'M', ',', 'I', 'j', '^', 'N', 'S', '_', 'y', '|',
             't', '\\', '\'', '\"'
     };
+
+    public static final byte[] BYTE_DICT_S = {
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+            'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+            'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
+            'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+            'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+            'Y', 'Z', '~', '`', '!', '@', '#', '$', '%', '^',
+            '&', '*', '(', ')', '_', '-', '+', '=', '[', ']',
+            '{', '}', ';', ':', ',', '.', '<', '>', '/', '?',
+            '\\', '|', '\'', '\"',
+    };
     private static final HashMap<Byte, Integer> BYTE_MAP = new HashMap<>();
+    private static final HashMap<Byte, Integer> BYTE_MAP_S = new HashMap<>();
     private static final HashMap<Integer, Byte> F_BYTE_MAP = new HashMap<>();
+    private static final HashMap<Integer, Byte> F_BYTE_MAP_S = new HashMap<>();
 
     static {
         for (int i = 0; i < BYTE_DICT.length; i++) {
             BYTE_MAP.put(BYTE_DICT[i], i);
         }
         BYTE_MAP.forEach((b, i) -> F_BYTE_MAP.put(i, b));
+
+        for (int i = 0; i < BYTE_DICT_S.length; i++) {
+            BYTE_MAP_S.put(BYTE_DICT_S[i], i);
+        }
+        BYTE_MAP_S.forEach((b, i) -> F_BYTE_MAP_S.put(i, b));
     }
 
     public static int byte2Radix(byte[] num, int radix) {
@@ -51,6 +71,40 @@ public class RadixUtil {
             bs[i] = bytes.get(i);
         }
         return bs;
+    }
+
+    public static int byte2Radix2(byte[] num, int radix) {
+        checkRadix(radix);
+        int length = num.length - 1;
+        int total = 0;
+        for (byte b : num) {
+            int i1 = getRadixNumS(b, radix);
+            total += (int) (i1 * Math.pow(radix, length));
+            length--;
+        }
+        return total;
+    }
+
+    public static byte[] radix2Byte2(final int num, int radix) {
+        checkRadix(radix);
+        ArrayList<Byte> bytes = new ArrayList<>(10);
+        int numCp = num;
+        do {
+            bytes.add(F_BYTE_MAP_S.get(numCp % radix));
+            numCp = numCp / radix;
+        } while (numCp != 0);
+        byte[] bs = new byte[bytes.size()];
+        for (int i = 0; i < bs.length; i++) {
+            bs[i] = bytes.get(i);
+        }
+        return bs;
+    }
+
+    public static int getRadixNumS(byte b, int radix) {
+        Integer i = BYTE_MAP_S.get(b);
+        if (i > radix)
+            throw new IllegalArgumentException("超出解析范围");
+        return i;
     }
 
     public static int getRadixNum(byte b, int radix) {
